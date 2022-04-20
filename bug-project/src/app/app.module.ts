8f3856079c0ca02c7e426b6b50979d4ac42bb104
StanislavLohtin <diffiller@gmail.com>
2022-04-20 13:52:27 +0200

    toggling diagram now creates memory leak

--- bug-project/src/app/app.module.ts
+++ bug-project/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SyncfusionDiagramComponent } from './syncfusion-diagram/syncfusion-diagram.component';
+import {DiagramModule} from "@syncfusion/ej2-angular-diagrams";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SyncfusionDiagramComponent } from './syncfusion-diagram/syncfusion-diag
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DiagramModule
   ],
   providers: [],
   bootstrap: [AppComponent]
